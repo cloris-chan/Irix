6dab1f97d22b28b1f6d63b6ad7a41054b456f1f2
Cloris Chan <chan@cloris.me>
2026-06-02 04:38:47 +0800

    Centralize internal visibility scope

--- RepositoryInternalsVisibleTo.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Irix.Core")]
-[assembly: InternalsVisibleTo("Irix.Platform")]
-[assembly: InternalsVisibleTo("Irix.Platform.Windows")]
-[assembly: InternalsVisibleTo("Irix.Rendering")]
-[assembly: InternalsVisibleTo("Irix.Poc")]
-[assembly: InternalsVisibleTo("Irix.Core.Tests")]
--- src/Irix.Core/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Irix.Rendering")]
-[assembly: InternalsVisibleTo("Irix.Poc")]
-[assembly: InternalsVisibleTo("Irix.Core.Tests")]
